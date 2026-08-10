bb403a991c86c4cb32d2af2c7e50b7c22915fcce
Narv6 <juan.narvaez2@estudiantesunibague.edu.co>
2026-08-10 15:50:18 -0500

    Update Main.java

--- src/mundo/Main.java
+++ src/mundo/Main.java
@@ -2,4 +2,10 @@ package mundo;
 
 public class Main {
 
-}
+	    public static void main(String[] args) {
+	        System.out.println("¡Hola Git!");
+	        System.out.println("Este es mi primer proyecto con control de versiones");
+	    }
+	}
+
+
