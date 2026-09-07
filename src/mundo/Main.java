ffd17412f05dba6d7dae01c568e1710b89571dda
Nyzroth <andress.mcharry@gmail.com>
2026-09-06 23:13:41 -0500

    Prueba commit
    hola pri

--- src/mundo/Main.java
+++ src/mundo/Main.java
@@ -1,7 +1,8 @@
 package mundo;
 
+// hola pri
 public class Main {
-
+		
 	    public static void main(String[] args) {
 	        System.out.println("¡Hola Git!");
 	        System.out.println("Este es mi primer proyecto con control de versiones");
